517e0ca5240c5a8574532cdb57a38ab1995a23ab
Nathan Larkin <nathanlarkin.usa@gmail.com>
2021-10-22 13:54:08 -0400

    improved error handling, added CLI arguments and file reading

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use std::error::Error;
+use std::fs;
+use std::process;
+
 // Special file to declare file structure
 pub mod components {
   pub mod actor;
@@ -14,3 +18,64 @@ pub mod grammar {
 
 pub mod utils;
 // pub mod world;
+
+pub struct Config {
+  pub filename: String,
+  pub quantity: i32,
+}
+impl Config {
+  pub fn new(args: &[String]) -> Result<Config, &str> {
+    if args.len() < 3 {
+      return Err("not enough arguments");
+    }
+
+    let filename = args[1].clone();
+    let quantity = match args[2].clone().parse::<i32>() {
+      Ok(i) => i,
+      Err(_e) => return Err("third argument was not an integer"),
+    };
+
+    Ok(Config { filename, quantity })
+  }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+  let contents = fs::read_to_string(config.filename)?;
+  let lines = &contents.split("\n").collect::<Vec<&str>>();
+  let parsed_rules = parse_file(&lines);
+  println!("{:#?}", parsed_rules);
+  // println!("With text:\n{}", contents);
+  Ok(())
+}
+
+fn parse_file(lines: &Vec<&str>) -> Vec<Rule> {
+  let mut rules = vec![];
+  for line in lines.iter() {
+    let built_rule = Rule::new(&line).unwrap_or_else(|err| {
+      println!("Problem parsing arguments: {}", err);
+      process::exit(1);
+    });
+    rules.push(built_rule);
+  }
+  rules
+}
+#[derive(Debug)]
+struct Rule {
+  left_hand: String,
+  right_hand: String,
+}
+
+impl Rule {
+  fn new(line: &str) -> Result<Rule, &str> {
+    let parsed = line.split("=").collect::<Vec<&str>>();
+    if parsed.len() < 2 {
+      return Err("line doesn't contain '=");
+    }
+    let left_hand = String::from(parsed[0].trim());
+    let right_hand = String::from(parsed[1].trim());
+    Ok(Rule {
+      left_hand,
+      right_hand,
+    })
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
 // use story_graph::components::{actor, location, world};
 // use story_graph::grammar::noun::noun;
 use std::collections::HashMap;
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::process;
 use story_graph::grammar::grammar;
+use story_graph::Config;
 
 // use crate::location;
 // use story_graph::utils;
 
 fn main() {
   // build_random_sentences(10);
-  // build_random_sentences(10);
+  let args: Vec<String> = env::args().collect();
+  // println!("{:?}", args);
+  // // build_random_sentences(10);
+  // let filename = &args[1];
+  // println!("In file {}", filename);
+  // let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+  // println!("With text:\n{}", contents);
+  let config = Config::new(&args).unwrap_or_else(|err| {
+    println!("Problem parsing arguments: {}", err);
+    process::exit(1);
+  });
+  println!("Genrating {} sentences.", config.quantity);
+  println!("In file {}", config.filename);
+
+  if let Err(e) = story_graph::run(config) {
+    println!("Application error: {}", e);
+    process::exit(1);
+  }
 }
 
 // fn print_sound<T: Animal>(t: &T) {
